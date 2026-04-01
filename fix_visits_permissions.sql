fb97d7b2faa1065beb20214d021fb055fec47e44
Manda Arolala ANDRIANINA <mandaarolalaa@gmail.com>
2026-04-01 08:58:18 +0300

    mise a jour du CSS

--- fix_visits_permissions.sql
+++
@@ -1,8 +0,0 @@
--- Supprimer l'ancienne politique restrictive
-DROP POLICY IF EXISTS "allow_select_visits" ON visits;
-
--- Créer une nouvelle politique qui autorise SELECT pour tout le monde (anon + authenticated)
-CREATE POLICY "allow_select_visits"
-  ON visits FOR SELECT
-  TO anon, authenticated
-  USING (true);
